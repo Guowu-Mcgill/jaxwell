6296452ca4c33d99bfc266531095f7e73e5583a0
Guowu Zhang <65426870+Guowu-Mcgill@users.noreply.github.com>
2020-12-14 21:37:15 -0500

    test
    test

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     install_requires=["numpy>=1.18.5", "jax>=0.2.4"],
-    url='https://github.com/stanfordnqp/jaxwell',
+    url='https://github.com/Guowu-Mcgill/jaxwell',
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'],
 )
